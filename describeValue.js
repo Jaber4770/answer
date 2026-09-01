1c2a15aa2053939d4486ded95f5b586709c8e6f1
Jaber4770 <jaber.ahmed4770@gmail.com>
2026-09-01 20:13:13 +0200

    problem one solved

---
+++ describeValue.js
@@ -0,0 +1,20 @@
+function describeValue(param) {
+
+    if (param) {
+        const type = typeof param;
+        const falsyTypes = [0, null, "", undefined, NaN, false];
+
+        if (falsyTypes.includes(param)) {
+            return `${type} | falsy`;
+        }
+        return `${type} | truthy`;
+
+    } else {
+        const type = typeof param;
+        return `${type} | falsy`;
+    }
+}
+
+
+const result = describeValue(undefined);
+console.log(result);
