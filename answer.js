5c929812a246a6b02d6ad7b2aa710d4551ab54ee
Jaber4770 <jaber.ahmed4770@gmail.com>
2026-09-01 22:04:14 +0200

    problem 4 done

--- answer.js
+++ answer.js
@@ -44,3 +44,53 @@ function getDayType(param) {
 
 
 // Question: 3
+
+function validateUsername(param) {
+    const username = param.toLowerCase();
+
+    if (username.length < 4) {
+        return "Too Short";
+    }
+    if (username.includes(" ")) {
+        return "No Space Allowed";
+    }
+    if (username.includes("admin")) {
+        return "Reserved Word";
+    }
+    return "Available"
+}
+
+
+
+// Question: 4
+
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+
+    let totalFare = 0;
+    if (distance <= 2 && distance > 0) {
+        totalFare = 50;
+    }
+
+    if (distance > 2) {
+        const remainingDistance = distance - 2;
+        const first2kmFare = 50;
+        const remainingKMfare = remainingDistance * 15;
+        const waitingFare = waitingMinutes * 2;
+        totalFare = first2kmFare + remainingKMfare + waitingFare;
+    }
+
+    if (isNight) {
+        totalFare = totalFare * 1.2;
+    }
+
+
+    return totalFare;
+}
+
+
+
+
+
+
+
+
---
+++ getCngfare.js
@@ -0,0 +1,25 @@
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+
+    let totalFare = 0;
+    if (distance <= 2 && distance > 0) {
+        totalFare = 50;
+    }
+
+    if (distance > 2) {
+        const remainingDistance = distance - 2;
+        const first2kmFare = 50;
+        const remainingKMfare = remainingDistance * 15;
+        const waitingFare = waitingMinutes * 2;
+        totalFare = first2kmFare + remainingKMfare + waitingFare;
+    }
+
+    if (isNight) {
+        totalFare = totalFare * 1.2;
+    }
+
+
+    return totalFare;
+}
+
+const result = getCngFare(5, true);
+console.log(result);
