801e7285b942c9010e3b4ed71a25aa0631bb38f3
Jaber4770 <jaber.ahmed4770@gmail.com>
2026-09-02 09:26:07 +0200

    all problem done

--- answer.js
+++ answer.js
@@ -90,7 +90,26 @@ function getCngFare(distance, isNight = false, waitingMinutes = 0) {
 
 
 // question 5: 
+const getChaseVerdict = (target, scored, ballsLeft) => {
 
+    const runsNeeded = target - scored;
+    if (runsNeeded <= 0) {
+        return "Won";
+    } else if (target > scored && ballsLeft <= 0) {
+        return "Lost";
+    }
+
+    const requiredRate = (runsNeeded / ballsLeft) * 6;
+    if (requiredRate <= 6 && requiredRate > 0) {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Comfortable`;
+    } else if (requiredRate > 6 && requiredRate <= 12) {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Tough`;
+    } else if (requiredRate > 12) {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Almost Impossible`;
+    }
+
+
+};
 
 
 
