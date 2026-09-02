784f237848daa910ec5508ac00ba98424413b650
Jaber4770 <jaber.ahmed4770@gmail.com>
2026-09-02 09:34:12 +0200

    question 1 corrected

--- describeValue.js
+++ describeValue.js
@@ -1,20 +1,14 @@
 function describeValue(param) {
 
+    const type = typeof param;
     if (param) {
-        const type = typeof param;
-        const falsyTypes = [0, null, "", undefined, NaN, false];
-
-        if (falsyTypes.includes(param)) {
-            return `${type} | falsy`;
-        }
         return `${type} | truthy`;
 
     } else {
-        const type = typeof param;
         return `${type} | falsy`;
     }
 }
 
 
-const result = describeValue(" ");
+const result = describeValue(NaN)
 console.log(result);
